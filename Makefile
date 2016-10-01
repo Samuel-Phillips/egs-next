a57b24d2258906fd9be0e7845026b1697df23ed6
Samuel Phillips <sam@samuelphillips.net>
2016-10-01 19:15:56 -0400

    Cleanup

--- Makefile
+++ Makefile
@@ -1,4 +1,7 @@
-.PHONY: all
+.PHONY: all clean
 
 all:
 	python3 mknext.py
+
+clean:
+	rm -f $(wildcard *next.user.js)
--- mrnnext.user.js
+++ mrnnext.user.js
@@ -1,17 +1,17 @@
 // ==UserScript==
-// @name        GPF Next
+// @name        Manga Reader.net Next Button
 // @namespace   samuelphillips.net
-// @description GPF Next
-// @include     http://www.gpf-comics.com/*
-// @include     https://www.gpf-comics.com/*
-// @require http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js
+// @description Next/Prev buttons for Manga Reader.net
+// @include     http://mangareader.net/*
+// @include     https://mangareader.net/*
+// @require     http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js
 // @version     1
 // @grant       none
 // ==/UserScript==
 
 $(function () {
-    var next = document.querySelector('.nav_link_forward a:first-child');
-    var prev = document.querySelector('.nav_link_back a:last-child');
+    var next = document.querySelector('.next');
+    var prev = document.querySelector('.prev');
 
     $(window).keydown(function (e) {
         if (e.keyCode === 39) {
@@ -23,4 +23,3 @@ $(function () {
         }
     });
 });
-
