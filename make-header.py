6eb4277b4c8c7ea19ac1ed04cc4bc8f1360e500b
Samuel Phillips <sam@samuelphillips.net>
2017-09-30 00:58:22 -0400

    etc

--- make-header.py
+++ make-header.py
@@ -13,6 +13,25 @@ template = """\
 // @grant       none
 // ==/UserScript==
 
+/*
+ * COPYRIGHT 2017 Samuel Phillips
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * The source code for this script can be found at the following URL:
+ * https://github.com/Samuel-Phillips/egs-next
+
 const site_conf = {site_conf};
 """
 
